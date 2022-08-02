3fc0f13f272b36bd6d9b8f792d284b41bf0c8bf7
Filip Cheliński <filipchel20@gmail.com>
2022-08-02 20:34:04 -0200

    Fixed mistakes in script.js

--- js/script.js
+++ js/script.js
@@ -8,6 +8,14 @@ buttonScissors.addEventListener('click', function(){ buttonClicked('nożyce'); }
 function buttonClicked(argButtonName) {
   clearMessages();
   console.log(argButtonName + ' został kliknięty');
+  
+  const playerMove = argButtonName;
+  console.log('ruch gracza to: ' + playerMove);
+  const randomNumber = Math.floor(Math.random() * 3 + 1);
+  console.log('wylosowana liczba to: ' + randomNumber);
+  const computerMove = getMoveName(randomNumber);
+  console.log('ruch komputera to: ' + computerMove);
+  displayResult(playerMove, computerMove);
 }
 
 function getMoveName(argMoveId) {
@@ -36,11 +44,3 @@ function displayResult(argPlayerMove, argComputerMove) {
   }
   printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 }
-
-const playerMove = argButtonName;
-console.log('ruch gracza to: ' + playerMove);
-const randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('wylosowana liczba to: ' + randomNumber);
-const computerMove = getMoveName(randomNumber);
-console.log('ruch komputera to: ' + computerMove);
-displayResult(playerMove, computerMove);
